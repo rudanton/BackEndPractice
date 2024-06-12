8fb3ed2ca018f6d9309e7c7b0e5758385e8f07e4
Antonio <rlaejrgh159@gmail.com>
2024-06-12 13:57:38 +0900

    404 처리.

---
+++ consoleIO/conditional.js
@@ -0,0 +1,11 @@
+var args = process.argv;
+console.log(args);
+
+if(args[2]=='1')
+    {
+        console.log('true')
+    }
+    else
+    {
+        console.log('false')
+    }
--- main.js
+++ main.js
@@ -2,45 +2,43 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var title = queryData.id;
-    console.log(queryData.id);
-    if(_url == '/'){
-      title = 'Welcome';
-    }
-    if(_url == '/favicon.ico'){
-        response.writeHead(404);
-        response.end();
-        return;
-    }
-    response.writeHead(200);
-    fs.readFile(`data/${title}`, 'utf8', (err, data)=>{
+var app = http.createServer(function (request, response) {
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var title = queryData.id;
+  var pathname = url.parse(_url, true).pathname;
+  if (pathname === '/') {
+    fs.readFile(`data/${title}`, 'utf8', (err, description) => {
       var template = `
       <!doctype html>
-    <html>
-    <head>
+      <html>
+      <head>
       <title>WEB1 - ${title}</title>
       <meta charset="utf-8">
-    </head>
-    <body>
+      </head>
+      <body>
       <h1><a href="/">WEB</a></h1>
       <ol>
-        <li><a href="/?id=HTML">HTML</a></li>
-        <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=JavaScript">JavaScript</a></li>
+      <li><a href="/?id=HTML">HTML</a></li>
+      <li><a href="/?id=CSS">CSS</a></li>
+      <li><a href="/?id=JavaScript">JavaScript</a></li>
       </ol>
       <h2>${title}</h2>
       <p>
-      ${data}
+      ${description}
       </p>
-    </body>
-    </html>
-    `
+      </body>
+      </html>
+      `;
+
+      response.writeHead(200);
       response.end(template);
-    })
-   
- 
+    });
+  }
+  else {
+    response.writeHead(404);
+    response.end('NOT FOUND');
+  }
+
 });
 app.listen(3000);
